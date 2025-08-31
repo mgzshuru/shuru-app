2966bdc36b72d198c4557e73e6d3ec418323b7de
abdelrahmanemadismail <abdelrahman.e.ismail@gmail.com>
2025-08-31 21:37:41 +0300

    feat(config): add absolute URL for OAuth providers in server configuration

--- server/config/server.ts
+++ server/config/server.ts
@@ -1,6 +1,8 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  // Add absolute URL for OAuth providers
+  url: env('SERVER_URL', 'https://cms.shuru.sa'),
   app: {
     keys: env.array('APP_KEYS'),
   },
